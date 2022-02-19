ba3d4dde84a039e332d91703394d0c74dfb12d39
likeroge <likeroge@gmail.com>
2022-02-19 12:46:57 +0400

    updated req.txt

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_from_directory, render_template
 from src.users import users_handlers
 from src.atran import atran_handlers
 from src.common import static_handler
@@ -9,6 +9,7 @@ from mongoengine import connect
 from pymongo import errors
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from werkzeug.wrappers import Response
+import os
 
 
 class App:
@@ -18,12 +19,20 @@ class App:
     debug: bool
     static_folder: str
     db_url: str
+    frontend_app: Flask
 
     def __init__(self, port: int, static: str, debug=True):
         self.port = port
         self.debug = debug
         self.static_folder = static
         self.app = Flask(__name__, static_folder=self.static_folder, static_url_path="")
+        self.frontend_app = Flask(__name__, static_folder='../front/build', static_url_path='')
+
+        @self.frontend_app.errorhandler(404)
+        def test_route(e):
+            print(e)
+            # return send_from_directory('../front/build', 'index.html')
+            return send_from_directory(self.frontend_app.static_folder, 'index.html')
         CORS(self.app)
 
     def add_db(self, db_url):
@@ -35,15 +44,20 @@ class App:
             print(errors.ConnectionFailure)
 
     def create_server(self):
-        self.app.wsgi_app = DispatcherMiddleware(Response('Not found', status=404), {'/api/v1': self.app.wsgi_app})
+        self.app.wsgi_app = DispatcherMiddleware(Response('Not found', status=404), {'/api/v1': self.app.wsgi_app,
+                                                                                     '': self.frontend_app})
+
+        # self.app.wsgi_app = DispatcherMiddleware(self.frontend_app)
+        # self.app.wsgi_app = DispatcherMiddleware(self.frontend_app, {'/api/v1': self.app})
+
         self.app.run(host="localhost", port=self.port, debug=self.debug)
 
     def create_router(self):
         routes = [
-            {
-                'name': "static",
-                'handler': static_handler.bp
-            },
+            # {
+            #     'name': "static",
+            #     'handler': static_handler.bp
+            # },
             {
                 'name': "users",
                 'handler': users_handlers.users
--- src/common/static_handler.py
+++ src/common/static_handler.py
@@ -1,12 +1,20 @@
 import os
-
-from flask import Blueprint, send_from_directory
-
-bp = Blueprint('static', __name__, url_prefix='/')
-
+from flask import Blueprint, send_from_directory, send_file
+# static_folder = os.path.join(os.path.dirname(__file__), '../../../front/build')
 static_folder = os.path.join(os.path.dirname(__file__), '../../../front/build')
+
+bp = Blueprint('static', __name__, url_prefix='/', static_folder=static_folder)
+
+
+# static_folder = os.path.join(os.path.dirname(__file__), '../../front/build')
+# static_folder = os.path.join(os.path.dirname(__file__), '../../../../front/build')
+# print(static_folder)
 
 
 @bp.route('/', methods=['GET'])
 def index():
-    return send_from_directory(static_folder, 'index.html')
+    print('INDEX')
+    print(bp.static_folder)
+    return bp.send_static_file('index.html')
+    # return send_from_directory(os.path.join(os.path.dirname(__file__), '../../../front/build'), 'index.html')
+
