3f26a8058dbbb24e6dc9b643923600a02b317a59
likeroge <likeroge@gmail.com>
2022-02-21 17:15:14 +0400

    updated req.txt

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ from src.config import config
 
 def main():
     static_folder = os.path.join(os.path.dirname(__file__), '../front/build')
-    # app = App(port=5001, static=static_folder)
-    app = App()
-
+    app = App(port=5001, static=static_folder)
     app.add_db(db_url=config.MONGO_URL)
     app.create_router()
     app.create_server()
