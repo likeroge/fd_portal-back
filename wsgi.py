349eb0e9f94e936068dc58b08d6b9d28bf92b971
likeroge <likeroge@gmail.com>
2022-02-21 17:18:10 +0400

    updated req.txt

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,19 @@
-from main import main
-from app import App
-from src.config import config
+# from main import main
+# from app import App
+# from src.config import config
+from flask import Flask
 
 if __name__ == '__main__':
-    static_folder = os.path.join(os.path.dirname(__file__), '../front/build')
-    app = App(port=5001, static=static_folder)
-    app.add_db(db_url=config.MONGO_URL)
-    app.create_router()
-    app.create_server()
+    # static_folder = os.path.join(os.path.dirname(__file__), '../front/build')
+    # app = App(port=5001, static=static_folder)
+    # app.add_db(db_url=config.MONGO_URL)
+    # app.create_router()
+    # app.create_server()
+
+    app = Flask(__name__)
+
+
+    @app.route("/")
+    def index():
+        return 'Hello, World!'
+
