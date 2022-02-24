f706e4826c3e5eda01f4a073356dcd22579857a6
likeroge <likeroge@gmail.com>
2022-02-25 02:33:28 +0400

    updated req.txt

--- src/config/config.py
+++ src/config/config.py
@@ -2,4 +2,4 @@
 # MONGO_URL = 'mongodb://egor:password@localhost:27017/FDops'
 # MONGO_URL = 'mongodb://egor:123qweQWE@193.187.175.206:27017/FDops'
 MONGO_URL = 'mongodb://egor:123qweQWE@localhost:27017/FDops'
-#sdssdsdsd
+#sdssdsdsdsdsd
